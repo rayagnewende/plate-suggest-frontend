60e73a011eec16a1946d010a88f8801a5c59c2a9
kabore r evariste <kaboreva@gmail.com>
2023-12-14 17:15:45 +0100

    nothing to do

--- App.js
+++ App.js
@@ -61,7 +61,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{ headerShown: false }}
-          initialRouteName="Preferencies"
+          initialRouteName="Home"
         >
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="SignIn" component={SignInScreen} />
